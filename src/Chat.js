fae1023de24a11a8495e999e46433b50ff87dc22
Jake Harris <harri676@purdue.edu>
2018-06-12 09:36:35 -0400

    Add main, sidebar, and chat components

---
+++ src/Chat.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Chat extends Component {
+    redner(){
+        return(
+            <div className="Chat">
+                Chat
+            </div>
+        )
+    }
+}
+
+export default Chat
---
+++ src/Sidebar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Sidebar extends Component {
+    redner(){
+        return(
+            <div className="Sidebar">
+                Sidebar
+            </div>
+        )
+    }
+}
+
+export default Sidebar
