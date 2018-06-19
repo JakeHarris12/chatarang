cc80bcbc90da699f01f3e6b056308e4c9b2bbfca
Jake Harris <harri676@purdue.edu>
2018-06-19 12:57:45 -0400

    Updates repo before starting homework

--- src/Main.js
+++ src/Main.js
@@ -57,7 +57,10 @@ class Main extends Component {
   }
 
   loadValidRoom = () => {
-    const realRoomName = Object.keys(this.state.rooms)[0]
+    const realRoomName = Object.keys(this.state.rooms).find(
+      roomName => this.state.rooms[roomName]
+    )
+
     this.props.history.push(`/rooms/${realRoomName}`)
   }
 
@@ -67,6 +70,7 @@ class Main extends Component {
         <Sidebar
           user={this.props.user}
           signOut={this.props.signOut}
+          users={this.props.users}
         />
         <Chat
           user={this.props.user}
